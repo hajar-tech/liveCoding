39cf38105b1e246531c96edff5fcdc1f0c447c69
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-07-22 17:26:29 +0100

    first commit

---
+++ src/main/java/com/livecoding/livecodingdto/LiveCodingDtoApplication.java
@@ -0,0 +1,13 @@
+package com.livecoding.livecodingdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveCodingDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveCodingDtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/livecoding/livecodingdto/LiveCodingDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livecoding.livecodingdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveCodingDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
